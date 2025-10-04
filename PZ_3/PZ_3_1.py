25d2563cd2711415cf175e266fa1af6a4c623667
BogdashaNedovolen <mih4ilov.bogd@yandex.ru>
2025-10-04 14:09:31 +0300

    bohdbyby pz_3_1.py

---
+++ PZ_3/PZ_3_1.py
@@ -0,0 +1,37 @@
+"""
+Даны два целых числа A и B. Проверить истинность высказывания: Ровно одно
+из чисел A и B чётное.
+"""
+
+try:
+    A = input('Введите число A: ')
+    B = input('Введите число B: ')
+    
+    while type(A) != int:
+        try:
+            A = int(A)
+        except Exception as e:
+            print('Неверный ввод числа..')
+            A = input('Введите число A: ')
+            
+    while type(B) != int:
+        try:
+            B = int(B)
+        except Exception as e:
+            print('Неверный ввод числа..')
+            B = input('Введите число B: ')
+            
+    
+    if (A % 2 == 0 and B % 2 != 0) or (A % 2 != 0 and B % 2 == 0):
+        print(f'Высказывание верно, одно из чисел A и B чётное.\nЧисла: {A} и {B}')
+    elif (A % 2 == 0 and B % 2 == 0):
+        print(f'Высказывание неверно, оба числа чётные.\nЧисла: {A} и {B}')
+    elif (A % 2 != 0 and B % 2 != 0):
+        print(f'Высказывание неверно, оба числа нечётные.\nЧисла: {A} и {B}.')
+    else:
+        raise ValueError()
+        
+except ValueError as e:
+    print('Неверные числа, попробуйте ещё раз.')
+except Exception as e:
+    print('Что-то пошло не так.')
