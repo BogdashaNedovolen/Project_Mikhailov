e52fa50bd68887b80075427188ab88377cb180d1
BogdashaNedovolen <mih4ilov.bogd@yandex.ru>
2025-12-06 12:23:06 +0300

    create 06.12 (PZ_5_DOP_3)

---
+++ PZ_5/DOP/3.py
@@ -0,0 +1,22 @@
+"""
+Написать программу, подсчитывающую количество цифр числа, используя функцию.
+"""
+
+def rasschoyt(n):
+    kolvo = 0
+    while n > 0:
+        n % 10
+        n //= 10
+        kolvo += 1
+    return kolvo
+
+n = input('Введите число: ')
+while type(n) != int:
+    try:
+        n = int(n)
+    except Exception as e:
+        print('Неверный ввод числа..')
+        n = input('Введите one: ')
+
+
+print(f'Количество цифр числа - {rasschoyt(n)}')
