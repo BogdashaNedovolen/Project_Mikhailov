cd902ec348209c4029adcf3805c3a1c5a215d82f
BogdashaNedovolen <mih4ilov.bogd@yandex.ru>
2026-01-16 17:39:04 +0300

    feat: create PZ_8_1

---
+++ PZ_8/1.py
@@ -0,0 +1,10 @@
+"""
+Проверьте наличие начения 200 в sample_dict = {'a': 100, 'b':200, 'c':300}.
+"""
+
+sample_dict = {'a': 100, 'b':200, 'c':300}
+
+if 200 in sample_dict.values():
+    print('Иметься')
+else:
+    print('Не иметься')
