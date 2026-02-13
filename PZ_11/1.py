5ec107f3e8dbc320ebfad8e8b9c924c3d3ec2952
BogdashaNedovolen <mih4ilov.bogd@yandex.ru>
2026-02-13 17:05:01 +0300

    Create PZ_11_1 (13.02)

---
+++ PZ_11/1.py
@@ -0,0 +1,20 @@
+"""
+функциональное программирование, решать только через него
+В последовательности на n целых чисел найти и вывести:
+- максимальный среди отрицательных
+- элементы кратные двум
+- их сумму
+"""
+
+from functools import reduce
+
+base = [3, -5, -1, 5, -3, 1, 8, -8]
+
+max_neg = reduce(lambda x, y: x if x > y else y, filter(lambda x: x < 0, base))
+print(max_neg)
+
+kratni = list(filter(lambda x: x % 2 == 0, base))
+print(kratni)
+
+summa = reduce(lambda x, y: x + y, base)
+print(summa)
